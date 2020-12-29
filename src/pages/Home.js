04138142b7b9cc9789be7a4deb95664922cb4ae2
abdulghaniasykurthoriq <abdulghaniasykurthoriq@gmail.com>
2020-12-29 10:25:13 +0700

    udah di update nih mas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div style={{'color':'white', 'display':'flex', 'justify-content':'center'}}>
             <div>
             <h1 style={{'text-decoration':'underline' }}>Begini cara menggunakan aplikasi todonya</h1>
-            <p>1. klik Navigasi todo yang ada di atas </p>
+            <p>1. klik Navigasi todo yang ada di atas     </p>
             <p>2. untuk menambahkan todo terus tinggal klik input yang ada di bawah kemudian klik tombol add</p>
             <p>3. untuk mengedit todo yang udah dibuat tinggal klik 2 kali aja ko di todo yang ingin diubah</p>
             <p>4. untuk menghapus todo tinggal klik tombol X yang ada di pojok todonya</p>
